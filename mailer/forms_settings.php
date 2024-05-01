5d4e320fec1084a673310e08284e49f627c2bf7a
RasansRise <158459365+RasansRise@users.noreply.github.com>
2024-05-01 16:32:59 +0300

    .

--- mailer/forms_settings.php
+++ mailer/forms_settings.php
@@ -1,7 +1,7 @@
 <?php
 	/* Forms settings */
 	$subj = "New message from 'Ruizarch'"; //letter subject
-	$to = 'yourname@domain.com'; // Enter Your E-mail
+	$to = 'gebraielmalak63@gmail.com'; // Enter Your E-mail
 	$from = 'admin@domain.com'; // Admin e-mail
 	$fromName = 'Ruizarch'; // Your company name
 	$charset = 'UTF-8';
